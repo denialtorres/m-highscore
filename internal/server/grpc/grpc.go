c644dd551beb4f3b0f3341fbfbcdd1f4743fd333
Daniel Torres <denial.torres@gmail.com>
2022-01-09 15:24:54 -0600

    apply internal server

--- internal/server/grpc/grpc.go
+++ internal/server/grpc/grpc.go
@@ -5,6 +5,8 @@ import (
   "google.golang.org/grpc"
   "context"
   "github.com/rs/zerolog/log"
+  "net"
+  "github.com/pkg/errors"
 )
 
 type Grpc struct {
@@ -13,6 +15,12 @@ type Grpc struct {
 }
 
 var HighScore = 99999999.0
+
+func NewServer(address string) *Grpc {
+  return &Grpc{
+    address: address,
+  }
+}
 
 func (g *Grpc) SetHighScore(ctx context.Context, input *pbhighscore.SetHighScoreRequest) (*pbhighscore.SetHighScoreResponse, error){
   log.Info().Msg("SetHighScore in m-highscore is called")
@@ -27,4 +35,28 @@ func (g *Grpc) GetHighScore(ctx context.Context, input *pbhighscore.GetHighScore
   return &pbhighscore.GetHighScoreResponse{
     HighScore: HighScore,
   }, nil
+}
+
+func (g *Grpc) ListenAndServe() error{
+  lis, err := net.Listen("tcp", g.address)
+
+  if err != nil {
+    return errors.Wrap(err, "failed to open tcp port")
+  }
+
+  serverOpts := []grpc.ServerOption{}
+
+  g.srv = grpc.NewServer(serverOpts...)
+
+  pbhighscore.RegisterGameServer(g.srv, g)
+
+  log.Info().Str("address", g.address).Msg("starting gRPC server for HighScore microservice")
+
+  err = g.srv.Serve(lis)
+
+  if err != nil{
+    return errors.Wrap(err, "failed to start gRPC server for HighScore MICROSERIVCE")
+  }
+
+  return nil
 }
